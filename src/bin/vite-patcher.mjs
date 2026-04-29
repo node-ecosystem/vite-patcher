a4e5baddd3f25c0242f64326eb52e7b44f3ea396
rtritto <ariele.1994@gmail.com>
2026-04-29 19:06:05 +0200

    fix: typo of vite-patches instead of vite-patcher

--- src/bin/vite-patcher.mjs
+++ src/bin/vite-patcher.mjs
@@ -7,12 +7,12 @@ const command = process.argv[2]
 const patchFunction = patches[command]
 
 if (!patchFunction) {
-  console.warn(`⚠️ Supported commands: "vite-patches ${Object.keys(patches).join(', ')}"`)
+  console.warn(`⚠️ Supported commands: "vite-patcher ${Object.keys(patches).join(', ')}"`)
 } else {
   try {
     await patchFunction()
   } catch (error) {
-    console.error(`❌ Error during the execution of the command: "vite-patches ${command}"`, error)
+    console.error(`❌ Error during the execution of the command: "vite-patcher ${command}"`, error)
   }
 }
 
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: {
-    'bin/vite-patches': 'src/bin/vite-patches.mjs'
+    'bin/vite-patcher': 'src/bin/vite-patcher.mjs'
   }
 })
