61182e3b851e9b2327e6b9f20e043c1455375b25
rtritto <ariele.1994@gmail.com>
2026-04-29 19:36:56 +0200

    fix: process exit code in bin

--- src/bin/vite-patcher.mjs
+++ src/bin/vite-patcher.mjs
@@ -8,12 +8,14 @@ const patchFunction = patches[command]
 
 if (!patchFunction) {
   console.warn(`⚠️ Supported commands: "vite-patcher ${Object.keys(patches).join(', ')}"`)
+  process.exit(1)
 } else {
   try {
     await patchFunction()
+    process.exit(0)
   } catch (error) {
     console.error(`❌ Error during the execution of the command: "vite-patcher ${command}"`, error)
+    process.exit(1)
   }
 }
 
-process.exit(1)
